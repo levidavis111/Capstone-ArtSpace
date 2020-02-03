c12c2d0d6ed1e822d5af285c77d8398b8fa2e5b3
karyRmartinez <karymartinez@pursuit.org>
2020-02-03 15:51:18 -0500

    cleaned up MainVC code

--- ArtSpaceDos/ViewControllers/MainVC.swift
+++ ArtSpaceDos/ViewControllers/MainVC.swift
@@ -1,11 +1,12 @@
 
-//
+
 import UIKit
 import SnapKit
 
+
 class HomePageVC: UIViewController {
   
-  //MARK: - Properties
+//MARK: - Properties
   var arrayOfImages = [UIImage(named: "1"),UIImage(named: "2"),UIImage(named: "3"),UIImage(named: "4"),UIImage(named: "5"),UIImage(named: "6"),UIImage(named: "7"),UIImage(named: "8"),UIImage(named: "9"),UIImage(named: "10")]
   
   var artObjectData = [ArtObject]() {
@@ -13,14 +14,10 @@ class HomePageVC: UIViewController {
       self.artCollectionView.reloadData()
     }
   }
-
-    //MARK: - Variables
-
-
-  //MARK: - Variables
+    
+//MARK: - Variables
   lazy var filterButton: UIButton = {
-
-    let button = UIButton()
+  let button = UIButton()
     button.setTitle("Filter", for: .normal)
     button.setTitleColor(.systemBlue, for: .normal)
     button.imageView?.contentMode = .scaleAspectFit
@@ -34,9 +31,7 @@ class HomePageVC: UIViewController {
     button.setImage(UIImage(systemName: "list.bullet"), for: .normal)
     button.imageView?.contentMode = .scaleToFill
     button.layer.cornerRadius = 10
-
     button.backgroundColor = .white
-//    button.imageEdgeInsets = UIEdgeInsets(top: 5,left: 5,bottom: 5,right: 5)
     button.tintColor = .black
     return button
   }()
@@ -46,14 +41,14 @@ class HomePageVC: UIViewController {
     let cv = UICollectionView(frame:.zero , collectionViewLayout: layout)
     layout.scrollDirection = .vertical
     layout.itemSize = CGSize(width: 250, height: 250)
-    cv.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
     cv.register(ArtCell.self, forCellWithReuseIdentifier: "artCell")
+    UIUtilities.setViewBackgroundColor(cv)
     cv.delegate = self
     cv.dataSource = self
     return cv
   }()
   
-  lazy var postButton: UIButton = {
+  lazy var createPost: UIButton = {
     let button = UIButton()
     button.setImage(UIImage(systemName: "plus"), for: .normal)
     button.imageView?.contentMode = .scaleAspectFit
@@ -64,7 +59,7 @@ class HomePageVC: UIViewController {
     return button
   }()
   
-  //MARK: -- Lifecycle
+//MARK: -- Lifecycle
   override func viewDidLoad() {
     super.viewDidLoad()
     UIUtilities.setViewBackgroundColor(view)
@@ -81,7 +76,7 @@ class HomePageVC: UIViewController {
     self.navigationController?.navigationBar.isHidden = false
   }
   
-  //MARK: - Obj-C Functions
+//MARK: - Obj-C Functions
   @objc func transitionToCreatePostVC() {
     let nextVC = CreatePost()
     self.navigationController?.pushViewController(nextVC, animated: true)
@@ -91,7 +86,7 @@ class HomePageVC: UIViewController {
     //code here
   }
   
-  //MARK: -- Private Functions
+//MARK: -- Private Functions
   private func getArtPosts() {
     FirestoreService.manager.getAllArtObjects { [weak self](result) in
       switch result {
@@ -105,41 +100,38 @@ class HomePageVC: UIViewController {
       }
     }
   }
-  //MARK: - UISetup
-  private func addSubviews() {
-
-      [filterButton,postButton,optionsMenu,artCollectionView].forEach({self.view.addSubview($0)})
+    
+//MARK: - UISetup Functions
+  private func addSubviews() {   [filterButton,createPost,optionsMenu,artCollectionView].forEach({self.view.addSubview($0)})
     view.bringSubviewToFront(optionsMenu)
   }
-  
 
-  
-  private func setupUIConstraints() {
-    postButton.snp.makeConstraints { make in
-      make.top.equalTo(self.view).offset(50)
-      make.right.equalTo(self.view).offset(-25)
-    }
-    
-    filterButton.snp.makeConstraints { (make) in
-      make.top.equalTo(self.view).offset(50)
-      make.left.equalTo(self.view).offset(25)
-    }
-    
-    optionsMenu.snp.makeConstraints { (make) in
-        make.bottom.equalTo(self.view).offset(-50)
-        make.right.equalTo(self.view).offset(-50)
-        make.width.equalTo(50)
-        make.height.equalTo(50)
-    }
-    artCollectionView.snp.makeConstraints { make in
+    private func setupUIConstraints() {
+        createPost.snp.makeConstraints { make in
+            make.top.equalTo(self.view).offset(50)
+            make.right.equalTo(self.view).offset(-25)
+        }
         
-        make.top.equalTo(postButton).offset(35)
-        make.left.equalTo(self.view)
-        make.bottom.equalTo(self.view)
-        make.right.equalTo(self.view)
+        filterButton.snp.makeConstraints { (make) in
+            make.top.equalTo(self.view).offset(50)
+            make.left.equalTo(self.view).offset(25)
+        }
+        
+        optionsMenu.snp.makeConstraints { (make) in
+            make.bottom.equalTo(self.view).offset(-50)
+            make.right.equalTo(self.view).offset(-50)
+            make.width.equalTo(50)
+            make.height.equalTo(50)
+        }
+        
+        artCollectionView.snp.makeConstraints { make in
+            make.top.equalTo(createPost).offset(35)
+            make.left.equalTo(self.view)
+            make.bottom.equalTo(self.view)
+            make.right.equalTo(self.view)
+        }
     }
-    }
-    }
+}
 
 //MARK: -- Extensions
 extension HomePageVC: UICollectionViewDataSource {
