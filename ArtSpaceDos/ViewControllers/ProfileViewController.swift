5a98cba564cf0165b4ae3a17fd4b18d5718fe685
karyRmartinez <karymartinez@pursuit.org>
2020-02-19 15:23:53 -0500

    UI objc layout

--- ArtSpaceDos/ViewControllers/ProfileViewController.swift
+++ ArtSpaceDos/ViewControllers/ProfileViewController.swift
@@ -10,23 +10,46 @@ import UIKit
 
 class ProfileViewController: UIViewController {
 
+    //MARK: UI OBJC
+    
+    lazy var userLabels: UILabel = {
+        let label = UILabel()
+        return label
+    }()
+    
+    lazy var manageAccount: UIButton = {
+        let button = UIButton()
+        return button
+    }()
+    
+    lazy var listingsLabel: UILabel = {
+        let label = UILabel()
+        return label
+    }()
+    
+    lazy var purchasedListLabel: UILabel = {
+        let label = UILabel()
+        return label
+    }()
+    
+    lazy var settingsButton: UIButton = {
+        let button = UIButton()
+        return button
+    }()
+    
+    lazy var signOut: UIButton = {
+        let button = UIButton()
+        return button
+    }()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
       self.navigationController?.navigationBar.isHidden = true
       UIUtilities.setViewBackgroundColor(view)
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
