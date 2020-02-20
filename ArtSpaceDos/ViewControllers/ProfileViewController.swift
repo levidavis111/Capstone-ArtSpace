529d3888af0b09a4fc06fa318d5b13ba8e259363
karyRmartinez <karymartinez@pursuit.org>
2020-02-20 15:18:01 -0500

    constraints and labels UI

--- ArtSpaceDos/ViewControllers/ProfileViewController.swift
+++ ArtSpaceDos/ViewControllers/ProfileViewController.swift
@@ -26,18 +26,24 @@ class ProfileViewController: UIViewController {
     }()
     lazy var profileImage: UIImageView = {
         let image = UIImageView()
-        image.image = UIImage(named: "profileIcon")
+        image.image = #imageLiteral(resourceName: "1")
         image.contentMode = .scaleAspectFill
-        image.layer.borderColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
-        image.layer.cornerRadius = 100
-               return image
+        image.layer.borderColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0)
+        image.layer.borderWidth = 5.0
+     var frame = image.frame
+        frame.size.width = 200
+        frame.size.height = 200
+        image.frame = frame
+   image.clipsToBounds = true
+        image.layer.cornerRadius = image.frame.size.width/2
+     return image
             }()
  
     lazy var addImageButton: UIButton = {
              let button = UIButton()
-             button.setTitle("Edit", for: .normal)
-             button.setTitleColor(.purple, for: .normal)
-             button.titleLabel?.font = UIFont(name: "Verdana-Bold", size: 12)
+             button.setTitle("+", for: .normal)
+        button.setTitleColor(.systemBlue, for: .normal)
+             button.titleLabel?.font = UIFont(name: "Verdana", size: 25)
              button.backgroundColor = .clear
              button.layer.cornerRadius = 5
              button.addTarget(self, action: #selector(addImagePressed), for: .touchUpInside)
@@ -48,6 +54,10 @@ class ProfileViewController: UIViewController {
     
     lazy var manageAccount: UIButton = {
         let button = UIButton()
+        button.setTitleColor(.systemBlue ,for: .normal)
+        button.setTitle("Manage Account", for: .normal)
+        button.backgroundColor = .clear
+        button.titleLabel?.font = UIFont(name: "Verdana", size: 15)
         return button
     }()
     
@@ -63,18 +73,38 @@ class ProfileViewController: UIViewController {
     
     lazy var settingsButton: UIButton = {
         let button = UIButton()
+        button.setTitle("Settings", for: .normal)
+        button.setTitleColor(.black, for: .normal)
+        button.titleLabel?.font = UIFont(name: "Verdana", size: 20)
         return button
     }()
     
     lazy var signOut: UIButton = {
         let button = UIButton()
+        button.setTitle("Sign Out", for: .normal)
+        button.setTitleColor(.black, for: .normal)
+        button.titleLabel?.font = UIFont(name: "Verdana", size: 15)
         return button
     }()
     
-    
+    //MARK: addSubviews
+    func addSubviews() {
+        view.addSubview(profileImage)
+        view.addSubview(addImageButton)
+        view.addSubview(manageAccount)
+        view.addSubview(settingsButton)
+        view.addSubview(signOut)
+    }
+    //MARK:ViewDidLoad cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-
+          addSubviews()
+        constrainProfilePicture()
+        constraintAddImage()
+        constraintManageAccountLabel()
+        settinglabelConstraints()
+        signOutConstraints()
+        
       self.navigationController?.navigationBar.isHidden = true
       UIUtilities.setViewBackgroundColor(view)
     }
@@ -117,8 +147,52 @@ class ProfileViewController: UIViewController {
 
     //MARK: Constraints
     
+    private func constrainProfilePicture() {
+        profileImage.snp.makeConstraints { (make) in
+            make.top.equalTo(self.view).offset(200)
+            make.centerX.equalTo(self.view)
+            make.height.equalTo(profileImage.frame.height)
+            make.width.equalTo(profileImage.frame.width)
+
+        }
+    }
+  
+    private func constraintAddImage() {
+          addImageButton.snp.makeConstraints { (make) in
+              make.top.equalTo(self.view).offset(200)
+            make.trailing.equalTo(self.view).offset(60)
+            //  make.centerX.equalTo(self.view)
+              make.size.equalTo(CGSize(width: 300, height: 300))
+        }
+      }
     
+    private func constraintManageAccountLabel() {
+        manageAccount.snp.makeConstraints { (make) in
+            make.top.equalTo(self.view).offset(400)
+            make.leading.equalTo(self.view).offset(50)
+            make.trailing.equalTo(self.view).offset(-50)
+            make.size.equalTo(CGSize(width: 100, height: 100))
+            
+        }
+    }
     
+    private func settinglabelConstraints() {
+        settingsButton.snp.makeConstraints { (make) in
+        make.bottom.equalTo(self.view).offset(-250)
+    make.leading.equalTo(self.view).offset(50)
+    make.trailing.equalTo(self.view).offset(-50)
+    make.size.equalTo(CGSize(width: 100, height: 100))
+                   
+        }
+    }
+    private func signOutConstraints() {
+        signOut.snp.makeConstraints { (make) in
+            make.bottom.equalTo(self.view).offset(-200)
+            make.leading.equalTo(self.view).offset(50)
+            make.trailing.equalTo(self.view).offset(-50)
+            make.size.equalTo(CGSize(width: 100, height: 100))
+        }
+    }
 
 }
 //MARK: Extension
@@ -132,3 +206,4 @@ extension ProfileViewController: UIImagePickerControllerDelegate, UINavigationCo
         dismiss(animated: true, completion: nil)
     }
 }
+ 
